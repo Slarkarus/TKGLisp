2c5b154a23b0314bd662fde30201eac80dd6b7a5
Slarkarus <slarkarus@gmail.com>
2025-10-02 18:49:14 +0300

    Update main.cpp for size test

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
+#include "types.hpp"
+#include "integer.hpp"
+
+using namespace tkg;
 
 int main(){
+    std::cout << sizeof(std::string*) << '\n';
+    std::cout << sizeof(tkg::Integer*) << '\n';
+    std::cout << sizeof(tkg::LispValueData) << '\n';
+    std::cout << sizeof(tkg::LispValueType) << '\n';
+    std::cout << sizeof(tkg::LispValue) << '\n';
+    std::cout << sizeof(tkg::ConsList) << '\n';
+    std::cout << sizeof(tkg::ConsList*) << '\n';
 
     return 0;
 }
