7cd9d67d680300a7c99e58a21121c24b25ab9770
Slarkarus <slarkarus@gmail.com>
2025-10-03 14:31:13 +0300

    Update test output in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -9,12 +9,15 @@ int main(){
     Value a = 4294967299;
     Value b = "Vasya Petrov";
     Value c = true;
-    Value d = List();
+    Value d = NIL;
     Value e = None;
     std::cout << a << '\n';
     std::cout << b << '\n';
     std::cout << c << '\n';
     std::cout << d << '\n';
     std::cout << e << '\n';
+
+    Value f = cons(nullptr, cons(12345, "Hello, world!"));
+    std::cout << f << '\n';
     return 0;
 }
