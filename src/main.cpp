2cb218ae55d48dc90b3608b6340f94a743b7b538
Slarkarus <slarkarus@gmail.com>
2025-10-03 16:50:07 +0300

    Add new examples of use types in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 #include "value.hpp"
 #include "integer.hpp"
 
@@ -6,19 +7,19 @@ using namespace tkg;
 
 int main()
 {
-    StoredType<int> x;
-    Value a = 4294967299;
-    Value b = "Vasya Petrov";
-    Value c = true;
-    Value d = NIL;
-    Value e = None;
-    std::cout << a << '\n';
-    std::cout << b << '\n';
-    std::cout << c << '\n';
-    std::cout << d << '\n';
-    std::cout << e << '\n';
+    std::vector<Value> arr;
 
-    Value f = cons(nullptr, cons(12345, "Hello, world!"));
-    std::cout << f << '\n';
+    arr.push_back(8235235);
+    arr.push_back(true);
+    arr.push_back("In the town");
+    arr.push_back(NIL);
+    arr.push_back(None);
+    arr.push_back(cons(None, cons(4294967299, cons("Where i was born!", NIL))));
+    arr.push_back(cons(NIL, cons("Lived a man", cons("Who sailed to sea", true))));
+
+    for (auto i : arr)
+    {
+        std::cout << i << '\n';
+    }
     return 0;
 }
