3721f43932031942cd14a2a210556591e42e9ab3
Slarkarus <slarkarus@gmail.com>
2025-10-15 04:08:44 +0300

    Add running repl in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,8 @@
-#include <iostream>
-#include <vector>
-#include "value.hpp"
-#include "integer.hpp"
-
-using namespace tkg;
+#include "repl.hpp"
 
 int main()
 {
-    std::vector<Value> arr;
-
-    arr.push_back(8235235);
-    arr.push_back(true);
-    arr.push_back("In the town");
-    arr.push_back(NIL);
-    arr.push_back(None);
-    arr.push_back(cons(None, cons(4294967299, cons("Where i was born!", NIL))));
-    arr.push_back(cons(NIL, cons("Lived a man", cons("Who sailed to sea", true))));
-
-    for (auto i : arr)
-    {
-        std::cout << i << '\n';
-    }
+    tkg::repl();
+    
     return 0;
 }
