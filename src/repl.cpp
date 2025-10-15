97ac08b2b63e14298814354f700c9fc98b4153fb
Slarkarus <slarkarus@gmail.com>
2025-10-15 04:08:14 +0300

    Add errors checking

--- src/repl.cpp
+++ src/repl.cpp
@@ -5,34 +5,73 @@
 #include "value.hpp"
 #include "parser.hpp"
 
-std::string read(){
+std::string read()
+{
     std::string result;
-    
+
     std::getline(std::cin, result);
 
     return result;
 }
 
-tkg::Value eval(tkg::Value input){
+tkg::Value eval(tkg::Value input)
+{
     return input;
 }
 
-void print(tkg::Value output){
+void print(tkg::Value output)
+{
     std::cout << output;
 }
 
-void tkg::repl(){
+void tkg::repl()
+{
     std::string s;
-    
-    while(true){
+
+    while (true)
+    {
         // Read
-        std::cin >> s;
+        std::string s = read();
         Parser parser(s);
         Value input = parser.parse();
 
-        
-        switch(parser.get_current_state()){
-            
+        ParserState current_state = parser.get_current_state();
+        switch (current_state)
+        {
+        case ParserState::Done:
+            break;
+        case ParserState::Empty:
+            std::cout << "Empty\n";
+            break;
+        case ParserState::Error:
+        {
+            ParserError current_error = parser.get_current_error();
+            switch (current_error)
+            {
+            case ParserError::MissingGlobalLeftBracket:
+                std::cout << "mglb\n";
+                break;
+            case ParserError::MissingLeftBracket:
+                std::cout << "mlb\n";
+                break;
+            case ParserError::MissingRightBracket:
+                std::cout << "mrb\n";
+                break;
+            case ParserError::MissingSecondDoubleQuote:
+                std::cout << "msdq\n";
+                break;
+            case ParserError::MissingSecondSemilicon:
+                std::cout << "mss\n";
+                break;
+            }
+            std::cout << "Error\n";
+            break;
+        }
+        case ParserState::Processing:
+            std::cout << "Processing\n";
+            break;
+        default:
+            break;
         }
 
         // Evaluate
@@ -40,6 +79,5 @@ void tkg::repl(){
 
         // Print
         std::cout << evaluated << '\n';
-
     }
 }
