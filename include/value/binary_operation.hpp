572d03d51d0f22d297250a77c51fd369ae86a282
Alexey Mayer <slarkarus@gmail.com>
2025-10-19 02:42:14 +0300

    Add enum BinaryOperation with cast to string

---
+++ include/value/binary_operation.hpp
@@ -0,0 +1,37 @@
+#ifndef TKG_VALUE_BINARY_OPERATION_HPP
+#define TKG_VALUE_BINARY_OPERATION_HPP
+
+#include "string_utils.hpp"
+
+namespace tkg
+{
+    enum class BinaryOperation{
+        Add,
+        Sub,
+        Mul, 
+        Div, 
+        Mod,
+        StrConcat
+    };
+
+    consteval std::string keyword_to_string(BinaryOperation operation)
+    {
+        switch (operation)
+        {
+        case BinaryOperation::Add:
+            return "_+_";
+        case BinaryOperation::Sub:
+            return "_-_";
+        case BinaryOperation::Mul:
+            return "_*_";
+        case BinaryOperation::Div:
+            return "_/_";
+        case BinaryOperation::Mod:
+            return "_%_";
+        case BinaryOperation::StrConcat:
+            return "_++_";
+        }
+    }
+}
+
+#endif // TKG_VALUE_BINARY_OPERATION_HPP
